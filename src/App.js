2f100fb53b32e6ee4b68923f1ae24da6fa7e223c
swarup saha <43001591+swarup-saha@users.noreply.github.com>
2022-02-02 23:44:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="root">
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
